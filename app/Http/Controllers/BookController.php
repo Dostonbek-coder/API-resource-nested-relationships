528cb4e1e64f14d9c66fdbbd8198070ad19e6c49
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-06-02 10:00:25 +0500

    All paginate() methods in BookController have been written correctly.

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BookResource;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::with('author')->paginate(10);
+        return BookResource::collection($books);
+    }
+}
